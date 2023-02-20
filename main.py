36b320779f6514c77c1172f88472949c7267f2ce
Samina Afroze <60169903+nipun747@users.noreply.github.com>
2023-02-20 12:36:14 +0000

    The value will be in integer format

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 weight=int(input("put your weight in kg:\n"))
 height=int(input("put your height in meter:\n"))
-BMI=str(weight/height**2)
-print("Your BMI is\n"+BMI)
+BMI=int(weight/height**2)
+bmi_org=str(BMI)
+print("Your BMI is\n"+bmi_org)
